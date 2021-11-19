71bcab5ff1df1bbae04a7575a80f271c1c24e3ee
Josiah Bruner <jbruner@duosecurity.com>
2021-11-19 10:57:58 -0500

    Refactor CFG to use a condensed (flat) data structure. Fixes #1

--- cfg.py
+++ cfg.py
@@ -2,9 +2,66 @@ import ast
 import typing
 import copy
 
+
+class InternalGraphRepesentation:
+    def __init__(self):
+        self._graph = {
+            '__narrow_entry__': {
+                'next': set()
+            }
+        }
+
+    def __str__(self):
+        return self._graph.__repr__()
+
+    # Adds a new node to the graph. context is used to find the path so far
+    # and next_node represents the name of the next node.
+    #
+    # Since right now the implementation only works on flat function calls,
+    # most of context is unused. Only the last element is used to find the
+    # caller.
+    #
+    # Duplicates are automatically prevneted, so you can call this without
+    # checking is_in_graph()
+    def add_node_to_graph(self, context: typing.List[str], next_node: str):
+        if len(context) == 0:
+            raise ValueError("context should never be empty. \
+            Use __narrow_entry__ for the root node")
+
+        caller = context[-1]
+
+        if caller not in self._graph:
+            raise ValueError("The caller should already exist in the graph")
+
+        self._graph[caller]['next'].add(next_node)
+
+        if next_node not in self._graph:
+            self._graph[next_node] = {
+                'next': set()
+            }
+
+    # Decides if context -> next_node already exists
+    # Since right now we only work on flat function calls, most of context is
+    # ignored.
+    def is_in_graph(self, context: typing.List[str], next_node: str):
+        if len(context) == 0:
+            raise ValueError("context should never be empty. \
+            Use __narrow_entry__ for the root node")
+
+        caller = context[-1]
+
+        if caller not in self._graph:
+            return False
+
+        if next_node in self._graph[caller]:
+            return True
+
+        return False
+
+
 class ControlFlowGraph:
     def __init__(self):
-        self._graph = {}
+        self._graph = InternalGraphRepesentation()
 
     # Function responsible for constructing a CFG given an entry file.
     #   only_file: If True, only builds a CFG contained in a single file.
@@ -13,15 +70,14 @@ class ControlFlowGraph:
         with open(file_path, mode='r') as source_content:
             syntax_tree: ast.Module = ast.parse(source_content.read(), file_path)
 
-            self._parse_and_resolve(syntax_tree, [])
+            self._parse_and_resolve(syntax_tree, ['__narrow_entry__'])
 
             print(self._graph)
 
     def _parse_and_resolve(self, ast_chunk: ast.AST,
                            context: typing.List[str]):
         if isinstance(ast_chunk, ast.Call):
-            if not self._in_graph(ast_chunk.func.attr, context):
-                self._add_node_to_graph(ast_chunk.func.attr, context)
+            self._graph.add_node_to_graph(context, ast_chunk.func.attr)
                 # TODO recurse
 
         # More work?
@@ -30,24 +86,3 @@ class ControlFlowGraph:
                 self._parse_and_resolve(child, context)
         elif hasattr(ast_chunk, 'value'):
             self._parse_and_resolve(ast_chunk.value, context)
-
-
-
-    # Check if we're already reached next_node_name
-    def _in_graph(self, next_node_name: str, context: typing.List[str]):
-        current = copy.deepcopy(self._graph)
-        for node in context:
-            current = current[node]
-
-        if next_node_name in current:
-            return True
-
-        return False
-
-    def _add_node_to_graph(self, next_node_name: str,
-                           context: typing.List[str]):
-        current = self._graph  # Get a reference
-        for node in context:
-            current = current[node]
-
-        current[next_node_name] = {}
