2c3a4c674b1d1aee954c68506b70092ad3fd2ec1
Josiah Bruner <jbruner@duosecurity.com>
2021-11-19 11:55:38 -0500

    Support for single-file recursive scanning. Fixes #2

--- cfg.py
+++ cfg.py
@@ -3,6 +3,18 @@ import typing
 import copy
 
 
+class FunctionDefVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self._funcs = {}
+
+    def visit_FunctionDef(self, node: ast.FunctionDef):
+        if node.name not in self._funcs:
+            self._funcs[node.name] = copy.deepcopy(node)
+
+    def get_functions(self):
+        return self._funcs
+
+
 class InternalGraphRepesentation:
     def __init__(self):
         self._graph = {
@@ -62,6 +74,7 @@ class InternalGraphRepesentation:
 class ControlFlowGraph:
     def __init__(self):
         self._graph = InternalGraphRepesentation()
+        self._root_tree = None
 
     # Function responsible for constructing a CFG given an entry file.
     #   only_file: If True, only builds a CFG contained in a single file.
@@ -69,6 +82,7 @@ class ControlFlowGraph:
     def construct_from_file(self, file_path: str, only_file=False):
         with open(file_path, mode='r') as source_content:
             syntax_tree: ast.Module = ast.parse(source_content.read(), file_path)
+            self._root_tree = copy.deepcopy(syntax_tree)
 
             self._parse_and_resolve(syntax_tree, ['__narrow_entry__'])
 
@@ -77,8 +91,17 @@ class ControlFlowGraph:
     def _parse_and_resolve(self, ast_chunk: ast.AST,
                            context: typing.List[str]):
         if isinstance(ast_chunk, ast.Call):
-            self._graph.add_node_to_graph(context, ast_chunk.func.attr)
-                # TODO recurse
+            func_name = None
+            if isinstance(ast_chunk.func, ast.Name):
+                func_name = ast_chunk.func.id
+            else:
+                func_name = ast_chunk.func.attr
+
+            self._graph.add_node_to_graph(context, func_name)
+            call_def = self._find_function_def_node(func_name)
+            if call_def:
+                self._parse_and_resolve(call_def,
+                                        context + [func_name])
 
         # More work?
         if hasattr(ast_chunk, 'body'):
@@ -86,3 +109,16 @@ class ControlFlowGraph:
                 self._parse_and_resolve(child, context)
         elif hasattr(ast_chunk, 'value'):
             self._parse_and_resolve(ast_chunk.value, context)
+
+    # Find a Function Definition node and return it, if possible
+    def _find_function_def_node(self, func_name: str):
+        starting_ast = self._root_tree
+
+        func_visitor = FunctionDefVisitor()
+        func_visitor.visit(starting_ast)
+
+        funcs = func_visitor.get_functions()
+        if func_name in funcs:
+            return funcs[func_name]
+
+        return None
---
+++ tests/single_file/main.py
@@ -0,0 +1,11 @@
+def foo():
+    print("Foo")
+    bar()
+
+def bar():
+    print("Bar")
+
+test = 102
+
+if test > 20:
+    foo()
